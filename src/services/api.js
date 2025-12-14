b3883485658abadc8a920a3b324cfee19919a60f
enesvarim <enesvarim@gmail.com>
2025-12-14 22:45:36 +0300

    backend bağlantı linki değişti

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://api.locaffy.store/api';
 
 export const publicApi = axios.create({
     baseURL: API_BASE_URL,
